af24a9b2bbf89e670440181b21ee671e6350de38
gianlucanied <gianluca.nieddu96@gmail.com>
2024-01-26 16:47:53 +0100

    Add: header per chiamata non funzionante

--- back/index.php
+++ back/index.php
@@ -2,4 +2,7 @@
 
 $lista_to_do = ["Fare spesa", "Portare il cane", "Chiamare mamma", "Scrivere al commercialista"];
 
-var_dump($lista_to_do);
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: http://localhost:5173/");
+
+echo json_encode($lista_to_do);
