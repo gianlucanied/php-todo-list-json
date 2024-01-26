2805e527591af5cb4b725541b050b85f356324a7
gianlucanied <gianluca.nieddu96@gmail.com>
2024-01-26 17:15:42 +0100

    Fix: call API

--- back/index.php
+++ back/index.php
@@ -3,6 +3,6 @@
 $lista_to_do = ["Fare spesa", "Portare il cane", "Chiamare mamma", "Scrivere al commercialista"];
 
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: http://localhost:5173/");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 
 echo json_encode($lista_to_do);
--- front/src/App.vue
+++ front/src/App.vue
@@ -2,16 +2,26 @@
 import axios from "axios";
 
 export default {
+  data() {
+    return {
+      liste: [],
+    };
+  },
   mounted() {
     axios
       .get("http://localhost:8080/php-todo-list-json/php-todo-list-json/back/")
       .then((res) => {
         console.log(res);
+        this.liste = res.data;
       });
   },
 };
 </script>
 
-<template></template>
+<template>
+  <ul>
+    <li v-for="lista in liste">{{ lista }}</li>
+  </ul>
+</template>
 
 <style scoped></style>
